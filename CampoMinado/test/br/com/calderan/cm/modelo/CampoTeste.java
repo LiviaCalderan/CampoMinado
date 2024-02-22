9c9e78e49c792bd09d062ed006277f924cfd8dd9
Lívia Calderan <liv_calderan@hotmail.com>
2024-02-22 19:51:39 -0400

    Testes do campo, metodo abrir, marcar

--- CampoMinado/test/br/com/calderan/cm/modelo/CampoTeste.java
+++ CampoMinado/test/br/com/calderan/cm/modelo/CampoTeste.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import br.com.calderan.cm.excecao.ExplosaoException;
+
 public class CampoTeste {
 	
 	private Campo campo;
@@ -61,5 +63,68 @@ public class CampoTeste {
 		
 		assertFalse(result);
 		
+	}
+	
+	@Test
+	void testeValorPadraoAtributoMarcado() {
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacaoDuasChamadas() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void abrirNaoMinadoNaoMarcado() {
+		assertTrue(campo.abrir());
+	}
+	
+	@Test
+	void abrirNaoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void abrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void abrirMinadoNaoMarcado() {
+		campo.minar();
+		
+		assertThrows(ExplosaoException.class, () -> {
+			campo.abrir();
+		});
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void abrirComVizinhos() {
+		Campo campo11 = new Campo(1, 1);
+		Campo campo12 = new Campo(1, 2);
+		campo12.minar();
+		
+		Campo campo22 = new Campo(2, 2);
+		campo22.addVizinho(campo11);
+		campo22.addVizinho(campo12);
+		
+		campo.addVizinho(campo22);
+		
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && campo11.isFechado());
 	}
 }
