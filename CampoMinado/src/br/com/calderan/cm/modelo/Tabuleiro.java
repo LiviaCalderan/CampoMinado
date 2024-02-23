fc65d51cb0d5fa7ee1dee9726499ba97d6dceb9a
Lívia Calderan <liv_calderan@hotmail.com>
2024-02-22 20:24:05 -0400

    Iniciando a classe Tabuleiro

--- CampoMinado/src/br/com/calderan/cm/modelo/Tabuleiro.java
+++ CampoMinado/src/br/com/calderan/cm/modelo/Tabuleiro.java
@@ -1,6 +1,45 @@
 package br.com.calderan.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
 	
+	private int qtdLinhas;
+	private int qtdColunas;
+	private int qtdMinas;
+	
+	private final List<Campo> campos = new ArrayList<Campo>();
+
+	public Tabuleiro(int qtdLinhas, int qtdColunas, int qtdMinas) {
+		this.qtdLinhas = qtdLinhas;
+		this.qtdColunas = qtdColunas;
+		this.qtdMinas = qtdMinas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+
+	private void gerarCampos() {
+		for (int i = 0; i < qtdLinhas; i++) {
+			for (int j = 0; j < qtdColunas; j++) {
+				campos.add(new Campo(i, j));
+			}
+		}
+	}
+	
+	private void associarVizinhos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.addVizinho(c2);
+			}
+		}
+	}
+	
+	private void sortearMinas() {
+	
+	}
 	
 }
